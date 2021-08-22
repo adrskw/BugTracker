6374048bb5a731279c1ec163152447bc2d467808
adrskw <48623654+adrskw@users.noreply.github.com>
2021-08-23 00:48:16 +0200

    Add CORS policy

--- src/BugTracker.API/Startup.cs
+++ src/BugTracker.API/Startup.cs
@@ -37,6 +37,14 @@ namespace BugTracker.API
 
             services.AddDbContextPool<ApplicationDbContext>(
                 options => options.UseSqlite(Configuration.GetConnectionString("BugTrackerDbConnection")));
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +58,8 @@ namespace BugTracker.API
             }
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
