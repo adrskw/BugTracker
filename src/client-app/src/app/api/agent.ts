e9c9e5a0c11a2ff6144339f84013f640aad51dd9
Adrian Skwarczyński <48623654+adrskw@users.noreply.github.com>
2021-09-26 10:41:31 +0200

    Fix formatting

--- src/client-app/src/app/api/agent.ts
+++ src/client-app/src/app/api/agent.ts
@@ -6,18 +6,18 @@ axios.defaults.baseURL = "http://localhost:5000/api";
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 const requests = {
-    get: <T>(url: string) => axios.get<T>(url).then(responseBody),
-    post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
-    put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
-    delete: <T>(url: string) => axios.delete<T>(url).then(responseBody),
+  get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+  post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
+  put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
+  delete: <T>(url: string) => axios.delete<T>(url).then(responseBody),
 }
 
 const Tickets = {
-    list: () => requests.get<Ticket[]>("/tickets")
+  list: () => requests.get<Ticket[]>("/tickets")
 }
 
 const agent = {
-    Tickets
+  Tickets
 }
 
 export default agent;
