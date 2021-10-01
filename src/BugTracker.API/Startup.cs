d4257cc8b0c2addfb9e31e0c36302bdc2d0686e3
Adrian Skwarczyński <48623654+adrskw@users.noreply.github.com>
2021-10-02 00:36:47 +0200

    Fix issue with deserialization of enum values from JSON

--- src/BugTracker.API/Startup.cs
+++ src/BugTracker.API/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using BugTracker.Core.Mapping;
 using BugTracker.Core.Tickets;
@@ -32,7 +33,11 @@ namespace BugTracker.API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "BugTracker.API", Version = "v1" });
