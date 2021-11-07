d898c7acf4689f291182f55c3c6d806393574dab
Adrian Skwarczyński <48623654+adrskw@users.noreply.github.com>
2021-11-08 00:43:01 +0100

    Redesign create/edit ticket form

--- src/client-app/src/features/tickets/form/TicketForm.tsx
+++ src/client-app/src/features/tickets/form/TicketForm.tsx
@@ -1,8 +1,9 @@
 import { observer } from 'mobx-react-lite';
 import React, { ChangeEvent, FormEvent, useState } from 'react';
-import { Button, Col, FloatingLabel, Form, Offcanvas, Row } from 'react-bootstrap';
+import { Button, Col, FloatingLabel, Form, Row } from 'react-bootstrap';
 import LoadingButtonContentComponent from '../../../app/layout/loading/LoadingButtonContentComponent';
 import { useStore } from '../../../app/stores/store';
+import { LinkContainer } from 'react-router-bootstrap';
 
 export default observer(function TicketForm() {
   const { ticketStore } = useStore();
@@ -38,65 +39,66 @@ export default observer(function TicketForm() {
 
     setTicket({ ...ticket, [name]: value });
   }
-  return (
-    <Offcanvas show={true} onHide={closeTicketForm} placement="end" className="largeOffcanvas">
-      <Offcanvas.Header closeButton>
-        <Offcanvas.Title>
-          {selectedTicket ? "Edit Ticket" : "Create Ticket"}
-        </Offcanvas.Title>
-      </Offcanvas.Header>
 
-      <Offcanvas.Body>
-        <Form onSubmit={handleSubmit}>
-          <Row className="g-3">
-            <Col xs={12}>
-              <FloatingLabel controlId="title" label="Title">
-                <Form.Control type="text" placeholder="title" name="title" value={ticket.title} onChange={handleInputChange} />
-              </FloatingLabel>
-            </Col>
-            <Col md={6}>
-              <FloatingLabel controlId="reporter" label="Reporter">
-                <Form.Control type="text" placeholder="reporter" name="reporter" value={ticket.reporter} onChange={handleInputChange} />
-              </FloatingLabel>
-            </Col>
-            <Col md={6}>
-              <FloatingLabel controlId="assignee" label="Assignee">
-                <Form.Control type="text" placeholder="assignee" name="assignee" value={ticket.assignee} onChange={handleInputChange} />
-              </FloatingLabel>
-            </Col>
-            <Col md={6}>
-              <FloatingLabel controlId="priority" label="Priority">
-                <Form.Select aria-label="priority" name="priority" onChange={handleSelectChange}>
-                  <option>choose priority</option>
-                  <option value="1">One</option>
-                  <option value="2">Two</option>
-                  <option value="3">Three</option>
-                </Form.Select>
-              </FloatingLabel>
-            </Col>
-            <Col md={6}>
-              <FloatingLabel controlId="status" label="Status">
-                <Form.Select aria-label="status" name="status" onChange={handleSelectChange}>
-                  <option>choose status</option>
-                  <option value="1">One</option>
-                  <option value="2">Two</option>
-                  <option value="3">Three</option>
-                </Form.Select>
-              </FloatingLabel>
-            </Col>
-            <Col xs={12}>
-              <FloatingLabel controlId="description" label="Description">
-                <Form.Control as="textarea" placeholder="describe the issue here" name="description" value={ticket.description} onChange={handleInputChange} />
-              </FloatingLabel>
-            </Col>
-            <Col xs={12}>
-              <Button variant="primary" type="submit" disabled={isProcessingRequest}>
-                {(isProcessingRequest) ? <LoadingButtonContentComponent /> : 'Submit'}
+  return (
+    <div className="ticket-view-container">
+      <div className="d-flex justify-content-between align-items-center mb-2">
+        <p className="h3">{ticket.id ? "Edit Ticket" : "Create Ticket"}</p>
+      </div>
+      <Form onSubmit={handleSubmit}>
+        <Row className="g-3">
+          <Col xs={12}>
+            <FloatingLabel controlId="title" label="Title">
+              <Form.Control type="text" placeholder="title" name="title" value={ticket.title} onChange={handleInputChange} />
+            </FloatingLabel>
+          </Col>
+          <Col md={6}>
+            <FloatingLabel controlId="reporter" label="Reporter">
+              <Form.Control type="text" placeholder="reporter" name="reporter" value={ticket.reporter} onChange={handleInputChange} />
+            </FloatingLabel>
+          </Col>
+          <Col md={6}>
+            <FloatingLabel controlId="assignee" label="Assignee">
+              <Form.Control type="text" placeholder="assignee" name="assignee" value={ticket.assignee} onChange={handleInputChange} />
+            </FloatingLabel>
+          </Col>
+          <Col md={6}>
+            <FloatingLabel controlId="priority" label="Priority">
+              <Form.Select aria-label="priority" name="priority" onChange={handleSelectChange}>
+                <option>choose priority</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </Form.Select>
+            </FloatingLabel>
+          </Col>
+          <Col md={6}>
+            <FloatingLabel controlId="status" label="Status">
+              <Form.Select aria-label="status" name="status" onChange={handleSelectChange}>
+                <option>choose status</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </Form.Select>
+            </FloatingLabel>
+          </Col>
+          <Col xs={12}>
+            <FloatingLabel controlId="description" label="Description">
+              <Form.Control as="textarea" placeholder="describe the issue here" name="description" value={ticket.description} onChange={handleInputChange} />
+            </FloatingLabel>
+          </Col>
+          <Col xs={12}>
+            <LinkContainer exact to="/tickets">
+              <Button variant="outline-secondary me-1">
+                Cancel
               </Button>
-            </Col>
-          </Row>
-        </Form>
-      </Offcanvas.Body>
-    </Offcanvas>
+            </LinkContainer>
+            <Button variant="primary" type="submit" disabled={isProcessingRequest}>
+              {(isProcessingRequest) ? <LoadingButtonContentComponent /> : 'Submit'}
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    </div >
   );
 });
