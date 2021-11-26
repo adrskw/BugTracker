0faab1d626dbec8c71fdc5a419f1a11bf0ee25bb
Adrian Skwarczyński <48623654+adrskw@users.noreply.github.com>
2021-11-26 22:17:23 +0100

    Add migration for custom IdentityUser

--- src/BugTracker.Persistence/Migrations/20211112181919_CustomIdentity.Designer.cs
+++ src/BugTracker.Persistence/Migrations/20211112181919_CustomIdentity.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using BugTracker.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BugTracker.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211112181919_CustomIdentity")]
+    partial class CustomIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
