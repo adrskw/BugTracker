614fdf866eee915b8976c77d2d3819a854314292
adrskw <48623654+adrskw@users.noreply.github.com>
2021-11-30 23:19:59 +0100

    Add Controller/DTOs for login and getting user info

---
+++ src/BugTracker.API/Controllers/AccountController.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using BugTracker.API.DTOs;
+using BugTracker.API.Services;
+using BugTracker.Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BugTracker.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly ITokenService tokenService;
+        private readonly ILogger<AccountController> logger;
+
+        public AccountController(UserManager<ApplicationUser> userManager,
+                                SignInManager<ApplicationUser> signInManager,
+                                ITokenService tokenService,
+                                ILogger<AccountController> logger)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.tokenService = tokenService;
+            this.logger = logger;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await userManager.FindByNameAsync(loginDto.Username);
+
+            if (user == null)
+                return Unauthorized();
+
+            var result = await signInManager.CheckPasswordSignInAsync(user: user,
+                                                                        password: loginDto.Password,
+                                                                        lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                return CreateUserDto(user);
+            }
+
+            return Unauthorized();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
+        {
+            var user = await userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+
+            return CreateUserDto(user);
+        }
+
+        private UserDto CreateUserDto(ApplicationUser user)
+        {
+            return new UserDto
+            {
+                Username = user.UserName,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Token = tokenService.CreateToken(user)
+            };
+        }
+    }
+}
---
+++ src/BugTracker.API/DTOs/UserDto.cs
@@ -0,0 +1,10 @@
+namespace BugTracker.API.DTOs
+{
+    public class UserDto
+    {
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Token { get; set; }
+    }
+}
